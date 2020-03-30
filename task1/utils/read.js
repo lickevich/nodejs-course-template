b2ccac52bed45102554337a6a38d7d527757aa62
Roman Lickevich <r.lickevich@gmail.com>
2020-03-30 15:58:28 +0200

    fix error messages

--- task1/utils/read.js
+++ task1/utils/read.js
@@ -9,36 +9,28 @@ const read = args => {
   const shiftToNumber = Number.parseInt(shift, 10);
 
   if (isNaN(shiftToNumber)) {
-    process.stderr.write(
-      'error: option "-s, --shift <num>", shift must be a number'
-    );
+    process.stderr.write('error: option "-s, --shift <num>" must be a number');
     process.exit(1);
   }
   if (shiftToNumber < 0 || shiftToNumber > 25) {
     process.stderr.write(
-      'error: option "-s, --shift <num>", shift must be from 0 to 25'
+      'error: option "-s, --shift <num>" must be from 0 to 25'
     );
     process.exit(1);
   }
   if (output && !isOutputPath) {
-    process.stderr.write(
-      'error: option "-o, --output", output path is not available'
-    );
+    process.stderr.write('error: option "-o, --output" path is not available');
     process.exit(1);
   }
   if (action !== 'encode' && action !== 'decode') {
-    process.stderr.write(
-      'error: option "-a, --action", action must be encode/decode'
-    );
+    process.stderr.write('error: option "-a, --action" must be encode/decode');
     process.exit(1);
   }
   if (input) {
     if (isInputPath) {
       return fs.createReadStream(input);
     }
-    process.stderr.write(
-      'error: option "-i, --input", input path is not available'
-    );
+    process.stderr.write('error: option "-i, --input" path is not available');
     process.exit(1);
   }
   return process.stdin;

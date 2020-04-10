b821a2093276beacf1add32d089f9583d71459f4
Roman Lickevich <r.lickevich@gmail.com>
2020-04-10 17:53:45 +0200

    delete comments

--- src/app.js
+++ src/app.js
@@ -24,6 +24,5 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
-// app.use('/boards/:boardId/tasks', taskRouter);
 
 module.exports = app;
